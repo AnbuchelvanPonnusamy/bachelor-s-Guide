b74bcf7c96c92d4c52a3a2637a3cbdbbeda0f973
Anbuchelvan Ponnusamy <anbuchelvan@5gwsit130.fifthgentech.local>
2016-04-05 11:02:31 +0530

    common

---
+++ RoomGuide/RGUtitlity/Common.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGUtitlity
+{
+    public class Common
+    {
+        public class NameValue
+        {
+            public string Name { get; set; }
+
+            public string Value { get; set; }
+        }
+    }
+    
+}

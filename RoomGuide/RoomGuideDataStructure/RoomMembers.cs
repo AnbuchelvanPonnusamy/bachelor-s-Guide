612070566d40b2b421a319fcb2292277a911aa0e
Anbuchelvan Ponnusamy <anbuchelvan@5gwsit130.fifthgentech.local>
2016-04-05 11:26:19 +0530

    dataStructure

---
+++ RoomGuide/RoomGuideDataStructure/RoomMembers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomGuideDataStructure
+{
+   public class RoomMembers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MailId { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Message { get; set; }
+        public string Id { get; set; }
+    }
+}
